496540e3e7c90cc4775a689d115c7fe89920f1f3
Johan Tibbelin <johan.roos@tibbelin.com>
2022-06-11 08:01:48 +0200

    ADC test

--- tools/logic_analyzer/logic_analyzer/src/main.cpp
+++ tools/logic_analyzer/logic_analyzer/src/main.cpp
@@ -3,8 +3,12 @@
 void setup() {
   // put your setup code here, to run once:
   pinMode(GPIOR10,INPUT);
+  Serial.begin(9600);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  analogRead(A0);
+  Serial.println(A0);
+  delay(1000);
 }
